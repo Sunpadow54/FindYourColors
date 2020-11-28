e2bf2e05619621a800a7835ab909846eae763861
Sunpadow <elsa.dessarps@gmail.com>
2020-11-28 19:38:44 +0100

    Modif : (style + html + js) before et after du bouton Keep/Drop

--- js/vue.js
+++ js/vue.js
@@ -20,55 +20,67 @@ mainColorOne.style.backgroundColor = randomRgba();
 mainColorTwo.style.backgroundColor = randomRgba();
 mainColorThree.style.backgroundColor = randomRgba();
 
-
-// gestion du click Switch Color
-const buttonRandomColor = document.getElementById('random_color');
-
-buttonRandomColor.addEventListener('click', function(){
-    canChangeColor(mainColorOne);
-    canChangeColor(mainColorTwo);
-    canChangeColor(mainColorThree);
-});
-
-
-// function qui verifie si la couleur est fixée et change la couleur
-function canChangeColor(theDiv){
-    if(!theDiv.classList.contains('fixed')){
-        theDiv.style.backgroundColor = randomRgba();
-        //change les couleurs des autres elements
-        changeColorAll(allColorOne, mainColorOne);
-        changeColorAll(allColorTwo, mainColorTwo);
-        changeColorAll(allColorthree, mainColorThree);
-    }
-}
-
-// Bouton Switch / Drop color
-document.querySelectorAll('.fix_color').forEach(item => {
-    item.addEventListener('click', function(){
-        //change text
-        if (this.textContent === 'Keep Color') {
-            this.textContent = 'Drop Color';
-        } else {
-            this.textContent = 'Keep Color';
-        }
-        //ajoute class fixed sur div de couleur parent
-        let divfixed = this.parentNode;
-        divfixed.classList.toggle('fixed');
-    });
+//affiche rgba
+document.querySelectorAll('p').forEach(item => {
+    item.textContent = item.parentNode.style.backgroundColor;
 })
 
-
-// change le reste des elements
+// change le reste des elements au chargement de la page
 const allColorOne = document.querySelectorAll('.color_one');
 const allColorTwo = document.querySelectorAll('.color_two');
 const allColorthree = document.querySelectorAll('.color_three');
 
-function changeColorAll(whereToChange, whereIsColor){
-    whereToChange.forEach( item => {
-        item.style.backgroundColor = whereIsColor.style.backgroundColor;
-    });
-}
 changeColorAll(allColorOne, mainColorOne);
 changeColorAll(allColorTwo, mainColorTwo);
 changeColorAll(allColorthree, mainColorThree);
 
+
+// function change la couleurs des autres blocs
+function changeColorAll(whereToChange, whereIsColor){
+    whereToChange.forEach( item => {
+        item.style.backgroundColor = whereIsColor.style.backgroundColor;
+    })
+}
+
+
+// gestion du click Switch Color
+
+const buttonRandomColor = document.getElementById('random_color');
+buttonRandomColor.addEventListener('click', function(){
+    changeColor(mainColorOne, allColorOne);
+    changeColor(mainColorTwo, allColorTwo);
+    changeColor(mainColorThree, allColorthree);
+});
+
+
+// function qui verifie si la couleur est fixée et change la couleur
+function changeColor(mainColorBloc, otherBlocs){
+    if(!mainColorBloc.classList.contains('fixed')){
+        mainColorBloc.style.backgroundColor = randomRgba();
+    }
+    changeColorAll(otherBlocs, mainColorBloc);
+    //affiche rgba
+document.querySelectorAll('p').forEach(item => {
+    item.textContent = item.parentNode.style.backgroundColor;
+})
+}
+
+
+// Bouton Keep / Drop color
+document.querySelectorAll('.fix_color').forEach(item => {
+    item.addEventListener('click', function(){
+        //change texte du bouton + before/after
+        if (this.textContent === 'Keep Color') {
+            this.textContent = 'Drop Color';
+            this.nextElementSibling.style.transform = 'translateX(-10px)';
+            this.previousElementSibling.style.transform = 'translateX(10px)';
+        } else {
+            this.textContent = 'Keep Color';
+            this.nextElementSibling.style.transform = 'translateX(0)';
+            this.previousElementSibling.style.transform = 'translateX(0)';
+        }
+        //ajoute class fixed sur son parent (main color)
+        let divfixed = this.parentNode;
+        divfixed.classList.toggle('fixed');
+    });
+})
