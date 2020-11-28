11c9756b4325ae64c0c62a8cd8ce4c58978c2c6d
Sunpadow <elsa.dessarps@gmail.com>
2020-11-28 20:38:25 +0100

    modif : affichage rgba

--- js/vue.js
+++ js/vue.js
@@ -20,11 +20,6 @@ mainColorOne.style.backgroundColor = randomRgba();
 mainColorTwo.style.backgroundColor = randomRgba();
 mainColorThree.style.backgroundColor = randomRgba();
 
-//affiche rgba
-document.querySelectorAll('p').forEach(item => {
-    item.textContent = item.parentNode.style.backgroundColor;
-})
-
 // change le reste des elements au chargement de la page
 const allColorOne = document.querySelectorAll('.color_one');
 const allColorTwo = document.querySelectorAll('.color_two');
@@ -33,6 +28,17 @@ const allColorthree = document.querySelectorAll('.color_three');
 changeColorAll(allColorOne, mainColorOne);
 changeColorAll(allColorTwo, mainColorTwo);
 changeColorAll(allColorthree, mainColorThree);
+changeRgbaText();
+
+
+// function change les couleurs (condition non 'fixed')
+function changeColor(mainColorBloc, otherBlocs){
+    if(!mainColorBloc.classList.contains('fixed')){
+        mainColorBloc.style.backgroundColor = randomRgba();
+    }
+    changeColorAll(otherBlocs, mainColorBloc);
+    changeRgbaText();
+}
 
 
 // function change la couleurs des autres blocs
@@ -42,9 +48,15 @@ function changeColorAll(whereToChange, whereIsColor){
     })
 }
 
+//Fonction affiche rgba
+function changeRgbaText(){
+    document.querySelectorAll('p').forEach(item => {
+        item.textContent = item.parentNode.style.backgroundColor;
+    })
+}
 
-// gestion du click Switch Color
 
+// Button Switch Colors
 const buttonRandomColor = document.getElementById('random_color');
 buttonRandomColor.addEventListener('click', function(){
     changeColor(mainColorOne, allColorOne);
@@ -53,30 +65,16 @@ buttonRandomColor.addEventListener('click', function(){
 });
 
 
-// function qui verifie si la couleur est fixée et change la couleur
-function changeColor(mainColorBloc, otherBlocs){
-    if(!mainColorBloc.classList.contains('fixed')){
-        mainColorBloc.style.backgroundColor = randomRgba();
-    }
-    changeColorAll(otherBlocs, mainColorBloc);
-    //affiche rgba
-document.querySelectorAll('p').forEach(item => {
-    item.textContent = item.parentNode.style.backgroundColor;
-})
-}
-
-
 // Bouton Keep / Drop color
 document.querySelectorAll('.fix_color').forEach(item => {
     item.addEventListener('click', function(){
         //change texte du bouton + before/after
         if (this.textContent === 'Keep Color') {
             this.textContent = 'Drop Color';
-            this.nextElementSibling.style.transform = 'translateX(-10px)';
             this.previousElementSibling.style.transform = 'translateX(10px)';
+            this.nextElementSibling.style.transform = 'translateX(-10px)';
         } else {
             this.textContent = 'Keep Color';
-            this.nextElementSibling.style.transform = 'translateX(0)';
             this.previousElementSibling.style.transform = 'translateX(0)';
         }
         //ajoute class fixed sur son parent (main color)
