fe5fbe1d97bdc5d2f97a9e160962888d5c0c2db3
Sunpadow <elsa.dessarps@gmail.com>
2020-12-18 13:41:11 +0100

    Add (vue.js) : copy rg + Refactor : dark/light functions

--- js/vue.js
+++ js/vue.js
@@ -1,17 +1,20 @@
-// DOM
-const buttonRandomColor = document.getElementById('random_color');
-const buttonKeepColor = document.querySelectorAll('.fix_color');
-const buttonDarkMode = document.getElementById('toogle_b_w');
-const main = document.getElementsByTagName('main')[0];
-const circle = document.getElementById('all_color_circle');
-const textRgbAndRgba = document.querySelectorAll('p');
-let bgColor;
+// --------------- Variables
 
+let darkMode = false;
+let bgColor;
+//creation popup Copy
+let popup = document.createElement('div');
+popup.classList.add('copy');
+document.getElementsByTagName('main')[0].parentNode.insertBefore(popup,this.nextSibling);
+
+
+// --------------- Define Colors
 
 // Random number > x
 let randomInt = (max) => {
     return Math.floor(Math.random() * (Math.floor(max) + 1));
 }
+
 
 // Color Contructor
 class Couleur {
@@ -36,8 +39,8 @@ class Couleur {
     }
 
     displayRgba(){
-        document.querySelectorAll('.' + this.name + ' > p').forEach(p => {
-            p.textContent = rgbaString(this.rgba);
+        document.querySelectorAll('.' + this.name + ' > .code_color').forEach(textRgba => {
+            textRgba.value = rgbaString(this.rgba);
         })
     }
 
@@ -64,23 +67,28 @@ class Couleur {
 }
 
 
-
-// Create 3 main Colors
+// Create 3 mains Colors
 let colorOne = new Couleur('one');
 let colorTwo = new Couleur('two');
 let colorThree = new Couleur('three');
-
 
 
 // Array of the main colors
 let couleurs = [];
 couleurs.push(colorOne,colorTwo,colorThree);
 
+//show rgb & colors of the circle
+findCurrentRgb();
+circleColor();
 
+
+
+
+// --------------- FUNCTIONS :
 
 // Show colors Circle
 function circleColor(){
-    circle.style.background =   'linear-gradient(217deg,' 
+    document.getElementById('all_color_circle').style.background =   'linear-gradient(217deg,' 
                                 + rgbaString(colorOne.rgba) + 
                                 ', transparent 70.71%), linear-gradient(127deg,' 
                                 + rgbaString(colorTwo.rgba) + 
@@ -90,8 +98,103 @@ function circleColor(){
 
 
 
+// Dark / Light Modes changes
+let toogleDarkLight = () => darkMode = !darkMode;
+
+function changeMode() {
+    toogleDarkLight();
+    findCurrentRgb();
+    //modify button dark/lightMode content
+    this.textContent = darkMode ? 'Light mode' : 'Dark mode';
+    /* modify css */
+    // colors
+    document.getElementsByTagName('body')[0].style.backgroundColor = darkMode ? 'black' : 'white';
+    document.getElementsByTagName('h1')[0].style.color = darkMode ? 'white' : 'black';
+    document.querySelectorAll('#colors_overlay .code_color').forEach(codeText => {
+        codeText.style.color = darkMode ? 'white' : 'black';
+    })
+    //icon button toogle
+    document.querySelector('.toogle_icon').style.background = darkMode ? 'center / contain url(../img/light-mode.svg) no-repeat' : 'center / contain url(../img/dark-mode.svg) no-repeat';
+}
+
+
+
+// Calculate a rgb (need background color & color on top of it)
+function calcRgb(bgColor, color) {
+    let resultR = Math.round(((1 - color.a) * bgColor.r) + (color.a * color.r));
+    let resultG = Math.round(((1 - color.a) * bgColor.g) + (color.a * color.g));
+    let resultB = Math.round(((1 - color.a) * bgColor.b) + (color.a * color.b));
+    let rgb = {
+        r : resultR,
+        g : resultG,
+        b : resultB
+    }
+    return rgb;
+}
+
+
+// Seek R G B of the background color (white or black)
+function backgroundColor() {
+    bgColor = darkMode ? { r : 255, g : 255, b : 255, a : 1 } : { r : 0, g : 0, b : 0, a : 1 };
+} 
+
+
+// convert R G B A values to string => rgba(r, g, b, a)
+function rgbaString(rgba) {
+    return 'rgba(' + rgba.r + ',' + rgba.g + ',' + rgba.b + ',' + rgba.a + ')';
+}
+
+
+// Convert R G B values to string => rgb(r, g, b)
+function rgbString(rgb) {
+    return 'rgb(' + rgb.r + ',' + rgb.g + ',' + rgb.b + ')';
+}
+
+
+// Show txt RGB of colors
+function showRgb(indexOftext, rgb){
+    document.querySelector('#colors_overlay > input:nth-of-type(' + indexOftext +'').value = rgbString(rgb);
+}
+
+
+// Calculate and display RGB of the colors
+function findCurrentRgb(){
+    backgroundColor();
+    showRgb(1, calcRgb(bgColor, colorOne.rgba));
+    showRgb(2, calcRgb(colorOne.rgba, colorTwo.rgba));
+    showRgb(3, calcRgb(bgColor, colorTwo.rgba));
+    showRgb(4, calcRgb(colorTwo.rgba, colorThree.rgba));
+    showRgb(5, calcRgb(bgColor, colorThree.rgba));
+}
+
+
+// copy colors codes
+function copy(text){
+    text.select();
+    document.execCommand('copy');
+}
+
+const copyMouseEnter = () => {
+    popup.style.display = 'block';
+    popup.style.background = 'center / contain no-repeat url(../img/icon-copy.svg)';
+}
+
+const copyMouseLeave = () =>  popup.style.display = 'none';
+
+function copyMouseClick(){
+    copy(this);
+    popup.style.background = 'center / contain no-repeat url(../img/icon-copied.svg)';
+     setTimeout(function() {
+        popup.style.display = 'none';
+      }, 500);
+}
+
+
+
+// --------------- AddEventListener of Buttons
+
 // Button Switch Colors
-buttonRandomColor.addEventListener('click', function(){
+document.getElementById('random_color').addEventListener('click', function(){
     couleurs.forEach(couleur => {
         couleur.changeColor();
         circleColor();
@@ -100,9 +203,8 @@ buttonRandomColor.addEventListener('click', function(){
 });
 
 
-
 // Button Keep / Drop color
-buttonKeepColor.forEach(item => {
+document.querySelectorAll('.fix_color').forEach(item => {
     item.addEventListener('click', function(){
         //change button txt & padlock (span before and span after positions)
         this.textContent = this.textContent === 'Keep Color' ? 'Drop Color' : 'Keep Color';
@@ -122,74 +224,20 @@ buttonKeepColor.forEach(item => {
 
 
 
-// function is dark or light mode
-function isDarkmode(){
-    if(buttonDarkMode.textContent === 'Dark mode'){
-        return true;
-    }
-}
-
-
-
 // Button Dark / White mode
-buttonDarkMode.addEventListener('click', function(){
-    //change button text
-    this.textContent = isDarkmode() ? 'Light mode' : 'Dark mode';
-    // change color of the background
-    main.style.backgroundColor = isDarkmode() ? 'white' : 'black';
-    // change the color of texts
-    textRgbAndRgba.forEach(item => {
-        item.style.color = isDarkmode() ? 'black' : 'white';
-    })
-    // recalculate rgb
-    findCurrentRgb();
+document.getElementById('toogle_b_w').addEventListener('click', changeMode);
+
+// Events on textarea RGBA
+document.querySelectorAll('.code_color').forEach(textRgba => { 
+    textRgba.addEventListener('click', copyMouseClick);
+    textRgba.addEventListener('mouseenter', copyMouseEnter);
+    textRgba.addEventListener('mouseleave', copyMouseLeave);
+
 })
 
-
-
-// Function calc rgb (need background color & color on top of it)
-function calcRgb(bgColor, color) {
-
-    let resultR = Math.round(((1 - color.a) * bgColor.r) + (color.a * color.r));
-    let resultG = Math.round(((1 - color.a) * bgColor.g) + (color.a * color.g));
-    let resultB = Math.round(((1 - color.a) * bgColor.b) + (color.a * color.b));
-
-    let rgb = {
-        r : resultR,
-        g : resultG,
-        b : resultB
-    }
-    return rgb;
-}
-
-
-// Show RGB of colors  / html
- function showRgb(indexOftext, rgb){
-    document.querySelector('#main_color_overlay > p:nth-of-type(' + indexOftext +'').textContent = rgbString(rgb);
-}
-
-// Seek R.G.B of the background color
-function backgroundColor() {
-    bgColor = isDarkmode() ? { r : 255, g : 255, b : 255, a : 1 } : { r : 0, g : 0, b : 0, a : 1 };
-} 
-
-// converti les valeurs R G B A en string => rgba(r, g, b, a)
-function rgbaString(rgba) {
-    return 'rgb(' + rgba.r + ',' + rgba.g + ',' + rgba.b + ',' + rgba.a + ')';
-}
-
-// converti les valeurs R G B en string => rgb(r, g, b)
-function rgbString(rgb) {
-    return 'rgb(' + rgb.r + ',' + rgb.g + ',' + rgb.b + ')';
-}
-
-// Calc and display RGB
-function findCurrentRgb(){
-    backgroundColor();
-    showRgb(1, calcRgb(bgColor, colorOne.rgba));
-    showRgb(2, calcRgb(colorOne.rgba, colorTwo.rgba));
-    showRgb(3, calcRgb(bgColor, colorTwo.rgba));
-    showRgb(4, calcRgb(colorTwo.rgba, colorThree.rgba));
-    showRgb(5, calcRgb(bgColor, colorThree.rgba));
-}
-findCurrentRgb();
+document.addEventListener('mousemove', function(event){
+            let x = event.clientX + 50;
+            let y = event.clientY - 70;
+            popup.style.left = x + 'px';
+            popup.style.top = y + 'px';
+})
